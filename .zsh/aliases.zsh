f55c975098eef8f1d9b8b0b457c47753bc3c22d8
gemrx <gemrdev@gmail.com>
2023-07-30 21:01:32 -0500

    updates

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,2 +1,3 @@
 alias ls="exa --color=always --icons --group-directories-first"
+alias tree="exa --tree --color=always --icons"
 alias onedrive="cd /home/gemr/Insync/gilberto.molinar1@utp.ac.pa/OneDrive\ Biz"
